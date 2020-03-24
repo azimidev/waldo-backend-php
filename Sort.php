9fcd83450749a7f97e8dbd1079e84f1c03e95250
Hassan Azimi <hassanazimi@users.noreply.github.com>
2020-03-24 22:39:27 +0000

    Add JsonMachine package and use it

--- Sort.php
+++ Sort.php
@@ -1,29 +1,39 @@
 <?php
 
+use JsonMachine\JsonMachine;
+
+require "./vendor/autoload.php";
+
 class Sort
 {
-    protected $fileName;
-    protected $items;
+	protected $fileName;
+	protected $items;
 
-    public function __construct($fileName)
-    {
-        $this->fileName = $fileName;
-    }
+	public function __construct($fileName)
+	{
+		$this->fileName = $fileName;
+	}
 
-    public function sort()
-    {
-        $fileContents = file_get_contents($this->fileName);
-        $this->items = json_decode($fileContents, true);
+	public function sort()
+	{
+		$this->items = JsonMachine::fromFile($this->fileName);
 
-        usort($this->items, function ($a, $b) {
-            return $a['registered'] <=> $b['registered'];
-        });
+		// foreach($fileContents as $file) {
+		// 	var_dump($file['registered']);
+		// }
 
-        return $this;
-    }
+		// $fileContents = file_get_contents($this->fileName);
+		// $this->items  = json_decode($fileContents, true);
 
-    public function getItems()
-    {
-        return $this->items;
-    }
+		usort($this->items, function($a, $b) {
+			return $a['registered'] <=> $b['registered'];
+		});
+
+		return $this;
+	}
+
+	public function getItems()
+	{
+		return $this->items;
+	}
 }
--- script.php
+++ script.php
@@ -7,20 +7,20 @@ set_time_limit(1200);
 require __DIR__ . '/Sort.php';
 
 $dataFiles = [
-    'small' => __DIR__ . '/data/junk-data.json',
-    'full' => __DIR__ . '/data/junk-data-full.json',
+	'small' => __DIR__ . '/data/junk-data.json',
+	'full'  => __DIR__ . '/data/junk-data-full.json',
 ];
 
 // Executes Fine
 $sort = new Sort($dataFiles['small']);
 $sort->sort();
 foreach ($sort->getItems() as $item) {
-    var_dump($item);
+	var_dump($item);
 }
 
 // Out of memory!
 $sort = new Sort($dataFiles['full']);
 $sort->sort();
 foreach ($sort->getItems() as $item) {
-    var_dump($item);
+	var_dump($item);
 }
