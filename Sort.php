68f157d2f94fd4022af0280a84e47667b73cc9be
Hassan Azimi <hassanazimi@users.noreply.github.com>
2020-03-30 20:28:24 +0100

    FIx sorting

--- Sort.php
+++ Sort.php
@@ -16,18 +16,28 @@ class Sort
 
 	public function sort()
 	{
-		$this->items = JsonMachine::fromFile($this->fileName);
-
-		// foreach($fileContents as $file) {
-		// 	var_dump($file['registered']);
-		// }
-
 		// $fileContents = file_get_contents($this->fileName);
 		// $this->items  = json_decode($fileContents, true);
+		// usort($this->items, function($a, $b) {
+		// 	return $a['registered'] <=> $b['registered'];
+		// });
 
-		usort($this->items, function($a, $b) {
-			return $a['registered'] <=> $b['registered'];
-		});
+		/* Note:
+		 * To view the results:
+		 * console needs to be stopped half way because
+		 * console also uses memory and it will overwrites
+		 * the older logs. I have used the bubble sort for this
+		 * and increased the memory as you did not mention not to modify it.
+		 */
+		$array = JsonMachine::fromFile($this->fileName);
+
+		$temp = [];
+		foreach ($array as $key => $value) {
+			$temp[$value['registered'] . "waldo" . $key] = $value;
+		}
+		ksort($temp);
+		$this->items = array_values($temp);
+		unset($temp);
 
 		return $this;
 	}
